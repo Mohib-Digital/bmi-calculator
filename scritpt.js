812e5c7f4b6207f4a217e032d9e125011040027a
Mohib-Digital <mohibullahafridi2020@gmail.com>
2024-10-07 16:08:52 +0500

    git few elements using js like 'heightInput, weightInput, calBtn and result

---
+++ scritpt.js
@@ -0,0 +1,6 @@
+const heightInput = document.getElementById('heightInput');
+const weightInput = document.getElementById('weightInput');
+const calculateButton = document.getElementById('calculateButton');
+const result = document.getElementById('result')
+
+
