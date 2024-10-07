f1a6ed1b0a75addc2d4cede615e226d12419a62f
Mohib-Digital <mohibullahafridi2020@gmail.com>
2024-10-07 16:12:27 +0500

    apply addEventListener method on calBtn, create variables for storing values of height and weight and for bmi result, put textContect into the result of bmi

--- scritpt.js
+++ scritpt.js
@@ -4,3 +4,14 @@ const calculateButton = document.getElementById('calculateButton');
 const result = document.getElementById('result')
 
 
+calculateButton.addEventListener('click', () => {
+  const height = heightInput.value;
+  const weight = weightInput.value;
+  const bmi = (weight / ((height / 100) * (height / 100))).toFixed(2) 
+  const bmiTotal = `Total BMI: ${bmi}`;
+  result.textContent = bmiTotal;
+  
+
+  
+
+})
