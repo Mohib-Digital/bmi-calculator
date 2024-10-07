89c2d79979e6baf022cfbf5e7c16b5026f6ccde0
Mohib-Digital <mohibullahafridi2020@gmail.com>
2024-10-07 16:15:47 +0500

    create a msg variable  and store msg element through js,  apply if else codition on bmi value

--- scritpt.js
+++ scritpt.js
@@ -12,6 +12,21 @@ calculateButton.addEventListener('click', () => {
   result.textContent = bmiTotal;
   
 
-  
+  const msg = document.getElementById('msg');
+
+  // BMI Conditions
+  if (bmi < 18.5){
+     msg.textContent = "Underweight"
+  }
+  else if(bmi >= 18.5 && bmi < 24.9){
+    msg.textContent = "Normal weight";
+  }
+  else if(bmi >= 25 && bmi < 29){
+    msg.textContent = "Overweight";
+  }
+  else if(bmi >= 30){
+    msg.textContent = "Obesity";
+  }
+
 
 })
